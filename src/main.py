0ab03567305b0085747395dd59eedf6905b5c2f0
Jakob <Jakob-Kein@web.de>
2023-04-03 20:17:08 +0200

    New routing

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 
 
 import flask
+from flask import render_template
+from pathlib import Path
 
 
 app = flask.Flask(__name__)
@@ -10,5 +12,9 @@ app = flask.Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("page1")
+def page1():
+    return render_template(str(Path(__file__).parent.parent / "pages/index.html"))
 
 
